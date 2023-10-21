4dc7cf4071f27749c8fec26be866181548f27b23
Yan Nalon <havercraft1@gmail.com>
2023-10-21 18:32:13 -0300

    Fix

--- Codigo/entregai/src/components/pages/supermarket/SupermarketManagement.tsx
+++ Codigo/entregai/src/components/pages/supermarket/SupermarketManagement.tsx
@@ -3,8 +3,6 @@ import { Supermarket } from "@/libs/types/Supermarket";
 import Registration from "../../modules/supermarket/management/Registration";
 import Visualizer from "../../modules/supermarket/management/Visualizer";
 import { useState } from 'react';
-import LazyPage from '@/libs/lazy/Lazy';
-import dynamic from 'next/dynamic';
 
 const SupermarketManagement = ({ systemSupermarkets }: { systemSupermarkets: Supermarket[] }) => {
 
