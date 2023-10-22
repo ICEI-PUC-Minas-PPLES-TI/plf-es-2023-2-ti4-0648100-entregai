477d129f7bb9f928a5c6097dc5861d84468ac027
Yan Nalon <havercraft1@gmail.com>
2023-10-22 03:56:08 -0300

    Order component

--- Codigo/entregai/src/libs/types/Order.ts
+++ Codigo/entregai/src/libs/types/Order.ts
@@ -1,7 +1,11 @@
 import { Buyer } from "./Buyer";
 import { Item } from "./Item";
+
+// Aguardando, Aprovado (ou Recusado), Em Transporte, Entregue
 export interface Order {
     date: string,
     buyer: Buyer,
+    paymentMethod: string,
     items: Item[]
+    status?: 0 | 1 | 2 | 3 | 4
 }
