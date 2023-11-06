5e42a41528be471698e7446cfde28f002aa801c7
Letícia Lott <103859841+0xLott@users.noreply.github.com>
2023-11-05 21:24:25 -0300

    Order tracking page styling #57

--- Codigo/entregai/src/components/modules/track/Tracker.tsx
+++ Codigo/entregai/src/components/modules/track/Tracker.tsx
@@ -1,17 +1,30 @@
-import { Button, TextField } from "@mui/material"
+import { ArrowForwardIos } from "@mui/icons-material"
+import { Button, IconButton, TextField } from "@mui/material"
 
 const Tracker = () => {
     return (
         <div>
-            <TextField 
-                id="orderCode" 
-                label="Código da Encomenda" 
+            <TextField
+                id="orderCode"
+                label="Código do pedido"
                 variant="outlined"
-                size="small" 
-                sx={{ mb: 2 }}
+                size="small"
+                sx={{ mb: 2, marginRight: '1rem', height: '2.5rem' }}
             />
 
-            <Button variant="contained">Buscar</Button>
+            <Button
+                variant="contained"
+                sx={{ height: '2.5rem' }}
+            >
+                Buscar
+                <IconButton
+                    size="small"
+                    edge="end"
+                    color="inherit"
+                >
+                    <ArrowForwardIos />
+                </IconButton>
+            </Button>
         </div>
     )
 }
