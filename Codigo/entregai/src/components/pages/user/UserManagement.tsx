b712980c00e4092ba04c5b5042e38ee13ab3d1dd
Letícia Lott <103859841+0xLott@users.noreply.github.com>
2023-11-07 06:12:34 -0300

    User management page styling

--- Codigo/entregai/src/components/pages/user/UserManagement.tsx
+++ Codigo/entregai/src/components/pages/user/UserManagement.tsx
@@ -3,17 +3,23 @@ import Registration from "../../modules/user/management/Registration"
 import Visualizer from "../../modules/user/management/Visualizer"
 import { Supermarket } from "@/libs/types/Supermarket"
 import { User } from "@/libs/types/User"
+import { Typography } from "@mui/material"
 
 const UserManagement = ({ systemUsers, systemSupermarkets }: { systemUsers: User[], systemSupermarkets: Supermarket[] }) => {
-    
-    const [ users, setUsers ] = useState<User[]>(systemUsers)
+
+    const [users, setUsers] = useState<User[]>(systemUsers)
 
     return (
         <div>
 
-            <Registration setUsers={setUsers} systemSupermarkets={systemSupermarkets} />
+            <Typography variant="h4" noWrap component="div" sx={{ padding: '1.5rem 0', fontWeight: 'fontWeightRegular' }}>
+                Usuários
+            </Typography>
 
             <Visualizer setUsers={setUsers} users={users} systemSupermarkets={systemSupermarkets} />
+
+            <Registration setUsers={setUsers} systemSupermarkets={systemSupermarkets} />
+
 
         </div>
     )
